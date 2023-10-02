57fccbb5c68d9ca9d9606697306e12b1e35ee6f3
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2023-10-03 00:18:41 +0200

    Add function for add book modal that adds book info to array

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+const myLibrary = [];
+
+
 const OpenAddModal = document.querySelector('#add_button');
-const AddBookModal = document.querySelector('.add_book_dialog');
 const CloseAddModal = document.querySelector('#add_book_cancel');
+const AddBookModal = document.querySelector('.add_book_dialog');
+const SubmitNewBook = document.querySelector('#add_book_submit');
+const BookTitle = document.querySelector('#book_title');
+const BookGenre = document.querySelector('#book_genre');
+const BookAuthor = document.querySelector('#book_author');
+const BookPages = document.querySelector('#book_pages');
+/* const BookStatus = document.querySelector('input[type="radio"][name="status"]:checked'); */
+const BookRating = document.querySelector('#book_rating');
+const BookCover = document.querySelector('#book_cover')
 
 // Add book modal
 OpenAddModal.addEventListener('click', () => {
@@ -20,6 +31,35 @@ AddBookModal.addEventListener('click', e =>{
 );
 CloseAddModal.addEventListener('click',() =>{
     AddBookModal.close();
-})
+});
+
+
+
 // Delete book modal
 // Still thikning how to do this
+
+function Book(title, genre, author, pages, /* status, */ rating, cover){
+    this.title = title;
+    this.genre = genre;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.rating = rating;
+    this.cover = cover;
+}
+
+function addBookToLibrary(){
+    let title = BookTitle.value;
+    let genre = BookGenre.value;
+    let author = BookAuthor.value;
+    let pages = BookPages.value;
+    /* let status = BookStatus.value; */
+    let rating = BookRating.value;
+    let cover = BookCover.value;
+
+    let newBook = new Book(title, genre, author, pages, /* status,*/ rating, cover);
+    myLibrary.push(newBook);
+}
+SubmitNewBook.addEventListener('click',() =>{
+    addBookToLibrary();
+});
